bc3eff282d9e8dad18e12e44a127950fec9a623c
Matthieu Brossard <matthieu.brossard@epita.fr>
2021-06-14 14:40:36 +0200

    howsmart little update

--- Commandes/howsmart.js
+++ Commandes/howsmart.js
@@ -1,7 +1,13 @@
 const Discord = require('discord.js');
+const config = require("../config.json");
 module.exports.run = (client, message, args) => {
     if ( args == "" ) {
-        message.channel.send( "<@" + message.author.id + ">" + "'s IQ = " + Math.floor(Math.random() * 255));
+        if(message.author.id == config.OwnerID){
+            message.channel.send( "<@" + message.author.id + ">" + "'s IQ = " + "infinity");
+        } else {
+            message.channel.send( "<@" + message.author.id + ">" + "'s IQ = " + Math.floor(Math.random() * 255));
+        }
+
     }
     else {
         message.channel.send(args+ "'s IQ is " + Math.floor(Math.random() * 255));
