1f45a4380ae2fd26647f2e1f2ee0d05011603bfb
Matthieu Brossard <matthieu.brossard@epita.fr>
2021-07-03 22:04:26 +0200

    loop reimplementation

--- Commands/loop.js
+++ Commands/loop.js
@@ -1,24 +1,17 @@
-module.exports.run =async (client, message, args) => {
-const play=require("./play");
-var found = false;
-for (i=0;i<play.length;i++){
-    if (message.guild.id==play[i].key){
-        var found = true;
-        play[i].value.loop=!play[i].value.loop;
-        if(play[i].value.loop){
-            loopInfo="Current Playlist is now looping"
-        } else {
-            loopInfo="Current Playlist is now not looping"
+module.exports.run =async (client, channel, authorID, args) => {
+    const ServerInfos = require("../ServerInfos").ServerInfos
+    return new Promise(function (resolve, reject){
+        for(let i=0;i<ServerInfos.length;i++){
+            if(ServerInfos[i].ID == channel.guild.id){
+                ServerInfos[i].Loop = !ServerInfos[i].Loop
+                if(ServerInfos[i].Loop){
+                    resolve("Playlist is now looping")
+                } else {
+                    resolve("Playlist is now not looping")
+                }
+            }
         }
-    }
-}
-if(!found){
-    message.reply("I do not have any playlist to loop on this server")
-}
-if (args[0]!=undefined){
-    loopInfo= loopInfo+"\nNote that this command doesn't require any argument."
-}
-message.channel.send(loopInfo)
+    })
 }
 module.exports.help = {
     name: 'loop',
