2620ff51db42c4a6e1f957df1b6897ab7aa8c9e9
Matthieu Brossard <matthieu.brossard@epita.fr>
2021-07-04 02:29:54 +0200

    removed some commented parts

--- Commands/queue.js
+++ Commands/queue.js
@@ -3,20 +3,9 @@ module.exports.run =async (client, channel, authorID, args) => {
         const ServerInfos = require ("../ServerInfos").ServerInfos;
         for (let i=0;i<ServerInfos.length;i++){
             if((channel.guild.id==ServerInfos[i].ID)){
-                let answered = false;
-                console.log("Server found")
                 let queueInfos ="Your current server Music Queue is:\n";
                 for(let q=0;q<ServerInfos[i].PlayList.length;q++){
                     queueInfos=queueInfos+(q+1)+": "+ServerInfos[i].PlayList[q].MusicTitle+"\n"
-                    /*if(queueInfos.length>1500){
-                        if(!answered){
-                            resolve(queueInfos)
-                            answered = true
-                        } else {
-                            channel.send(queueInfos)
-                        }
-                        queueInfos=""
-                    }*/
                 }
                 if(ServerInfos[i].PlayList.length==0){
                     console.log("Playlist is empty")
@@ -30,14 +19,7 @@ module.exports.run =async (client, channel, authorID, args) => {
                 if(ServerInfos[i].CurrentSong!=null){
                     queueInfos+="\nCurrently playing: "+ServerInfos[i].CurrentSong.MusicTitle
                 }
-                //if(!answered){
-                    //console.log("not answered")
-                    resolve(queueInfos)
-                //} else {
-                    //console.log("answered")
-                    //channel.send(queueInfos)
-                //}
-
+                resolve(queueInfos)
             }
 
         }
